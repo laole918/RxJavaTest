2608fc3b728749a2a57633b54542c31fef0db70f
laole918 <laole918@qq.com>
2016-03-11 18:44:34 +0800

    添加rxjava rxandroid

--- app/src/main/java/com/laole918/rxjavatest/MainActivity.java
+++ app/src/main/java/com/laole918/rxjavatest/MainActivity.java
@@ -8,6 +8,12 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,24 @@ public class MainActivity extends AppCompatActivity {
         new Thread(() -> {
             Log.e("lambda", "lambda in thread");
         }).start();
+
+//        Observable.from(folders)
+//                .flatMap((Func1) (folder) -> { Observable.from(file.listFiles()) })
+//                .filter((Func1) (file) -> { file.getName().endsWith(".png") })
+//                .map((Func1) (file) -> { getBitmapFromFile(file) })
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe((Action1) (bitmap) -> { imageCollectorView.addImage(bitmap) });
     }
+
+//    public class LiftAllTransformer implements Observable.Transformer<Integer, String> {
+//        @Override
+//        public Observable<String> call(Observable<Integer> observable) {
+//            return observable
+////                    .lift()
+//                    .lift2()
+//                    .lift3()
+//                    .lift4();
+//        }
+//    }
 }
