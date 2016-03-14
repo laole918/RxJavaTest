6b7f5ef45de299c5783f0c983c6a58f9e015d97e
laole918 <laole918@qq.com>
2016-03-14 08:17:13 +0800

    测试1

--- app/src/main/java/com/laole918/rxjavatest/MainActivity.java
+++ app/src/main/java/com/laole918/rxjavatest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.laole918.rxjavatest;
 
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 
 import org.androidannotations.annotations.AfterViews;
@@ -9,10 +8,7 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
+import rx.Subscriber;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -22,28 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     protected void afterViews() {
-        button.setOnClickListener(view -> Log.e("lambda", "lambda"));
-        new Thread(() -> {
-            Log.e("lambda", "lambda in thread");
-        }).start();
-
-//        Observable.from(folders)
-//                .flatMap((Func1) (folder) -> { Observable.from(file.listFiles()) })
-//                .filter((Func1) (file) -> { file.getName().endsWith(".png") })
-//                .map((Func1) (file) -> { getBitmapFromFile(file) })
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe((Action1) (bitmap) -> { imageCollectorView.addImage(bitmap) });
+        button.setOnClickListener(view -> onClickButton());
     }
 
-//    public class LiftAllTransformer implements Observable.Transformer<Integer, String> {
-//        @Override
-//        public Observable<String> call(Observable<Integer> observable) {
-//            return observable
-////                    .lift()
-//                    .lift2()
-//                    .lift3()
-//                    .lift4();
-//        }
-//    }
+    private void onClickButton() {
+        Observable.just("Hello, world!")
+                .map(s -> s.hashCode())
+                .map(i -> String.valueOf(i))
+                .subscribe(s -> System.out.println(s), e -> e.printStackTrace(), () -> {
+                    System.out.println("完成");
+                });
+//        Observable.
+    }
 }
